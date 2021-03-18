2dcfdeae9e3e6bad3ccb03a0e0dbd2be0dcc8c29
root <xhkrasniqi98@gmail.com>
2021-03-18 19:36:20 +0100

    launchervbs

--- kallxo_rastet.py
+++ kallxo_rastet.py
@@ -62,13 +62,15 @@ def msg_notification(lista_rasteve):
     return "Te infektuar " + str(lista_rasteve[0]) + ". Te sheruar " + str(lista_rasteve[1]) + ". Te vdekur " + str(lista_rasteve[2])
 
 if __name__ == '__main__':
-    rastet_net = get_rastet_net()
-    rastet_file = read_rastet()
-    for i in range(3):
-        if rastet_file[i] != rastet_net[i]:
-            write_rastet(rastet_net)
-            send_notification(msg_notification(read_rastet()))
-            break
-    else:
-        print('No, Update.')
-    
+    while True:
+        rastet_net = get_rastet_net()
+        rastet_file = read_rastet()
+        for i in range(3):
+            if rastet_file[i] != rastet_net[i]:
+                write_rastet(rastet_net)
+                send_notification(msg_notification(read_rastet()))
+                break
+        else:
+            print('No, Update.')
+        time.sleep(5000)
+        
---
+++ launcher.vbs
@@ -0,0 +1,2 @@
+Set oShell = CreateObject (“WScript.Shell”) 
+oShell.run “pythonw kallxo_rastet.py”
